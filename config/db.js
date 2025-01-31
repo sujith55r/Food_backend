23fb7f3cd5cee8dc9b01e5e67477dc0bcdaf3310
Sujith <rsujith445@gmail.com>
2025-01-31 12:56:50 -0500

    first commit

--- config/db.js
+++ config/db.js
@@ -2,6 +2,6 @@ import mongoose from "mongoose";
 
 
 export const connectDB=async()=>{
-    await mongoose.connect('mongodb+srv://sujith:9342158635@cluster0.ptp7n.mongodb.net/food-del').then(()=>console.log("DB Connected"));
+    await mongoose.connect(process.env.DATABASE).then(()=>console.log("DB Connected"));
 }
 
--- middleware/auth.js
+++ middleware/auth.js
@@ -9,7 +9,7 @@ const authMiddleware=async(req,res,next)=>{
         const token_decode=jwt.verify(token,process.env.JWT_SECRET);
         req.body.userId=token_decode.id;
         next();
-    } catch (error) {
+    } catch (error) { 
         console.log(error);
         res.json({success:false,message:"Error"})
     }
